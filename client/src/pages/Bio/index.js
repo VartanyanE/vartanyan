beb165c1941b66e47781231b9fc518027e801119
VartanyanE <mike.vartanyan@yahoo.com>
2020-11-17 22:01:26 -0800

    animations

--- client/src/pages/Bio/index.js
+++ client/src/pages/Bio/index.js
@@ -3,9 +3,17 @@ import { animated, useSpring } from "react-spring";
 import "./style.scss";
 
 function Bio(props) {
+  useEffect(() => {
+    setToggle(true);
+  }, []);
   const [main, setMain] = useState(false);
+  const [toggle, setToggle] =useState(false);
   const scale = useSpring({
     transform: main ? " scale(1.2) " : `scale(1)`,
+  });
+  const animate = useSpring({
+    config: { duration: 950, tension: 400, mass: 5 },
+    transform: toggle ? `translate3d(0,0,0) ` : `translate3d(100%, 0,0) `,
   });
   const myGithub = () => window.open("https://github.com/VartanyanE", "_blank");
   const myLinkedin = () => {
@@ -27,9 +35,26 @@ function Bio(props) {
           onMouseLeave={() => setMain(!main)}
           style={scale}
         >
-          <div>JUST</div>
-          <div>A</div>
-          <div>SOFTWARE</div>
+          <animated.div style={animate} style={{display: "inline",}}>JUST&nbsp;</animated.div>
+          <div   style={{display: "inline",}}>A&nbsp;</div>
+          <div   style={{display: "inline",}}>SOFTWARE ENGINEER&nbsp;</div>
+          <div   style={{display: "inline",}}>WHO&nbsp;</div><br/>
+          <div   style={{display: "inline",}}>LOVES&nbsp;</div>
+          <div   style={{display: "inline",}}>TO&nbsp;</div>
+          <div   style={{display: "inline",}}>CODE.&nbsp;&nbsp;</div>
+          <div   style={{display: "inline",}}>SKILLS&nbsp;</div>
+          <div   style={{display: "inline",}}>I&nbsp;</div>
+          <div   style={{display: "inline",}}>HAVE&nbsp;</div> <br/>
+          <div   style={{display: "inline",}}>MASTERED&nbsp;</div>
+          <div   style={{display: "inline",}}>ARE&nbsp;&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>HTML,&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>CSS,&nbsp;</div><br/>
+          <div   style={{display: "inline", fontSize: "28px"}}>JAVASCRIPT,&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>REACT,&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>NODE,&nbsp;</div><br/>
+          <div   style={{display: "inline", fontSize: "28px"}}>EXPRESS,&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>MONGODB,&nbsp;</div>
+          <div   style={{display: "inline", fontSize: "28px"}}>GIT.</div>
         </animated.div>
       </div>
     </>
