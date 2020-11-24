a907ddd679a235ae96123f724cf7083ec8167c04
VartanyanE <mike.vartanyan@yahoo.com>
2020-11-23 22:18:36 -0800

    project

--- client/src/pages/Projects/index.jsx
+++ client/src/pages/Projects/index.jsx
@@ -16,9 +16,11 @@ function Projects(props) {
         <div className="project-heading">
           <h1>PROJECTS</h1>
         </div>
-        <div className="project-main"></div>
-        <div className="project-main"></div>
-        <div className="github" onClick={myGithub}></div>
+        <div className="project-main">
+          <div className="planthood" onClick={()=> window.open("http://planthood.herokuapp.com/", "_blank")}><h3>PLANTHOOD</h3> <br/> </div>
+        </div>
+        <div className="project-main">  <div className="palmbuddy" onClick={()=> window.open("http://palmbuddy.herokuapp.com/", "_blank")}><h3 style={{opacity:0}}>--------------------</h3> <br/> </div></div>
+        <div className="github" onClick={ () => window.open("https://github.com/VartanyanE", "_blank")}></div>
       </div>
     </>
   );
