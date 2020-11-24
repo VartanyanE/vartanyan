3f805ad1b14f00e6208416b950bb0324f6f559c7
vartanyanE <mike.vartanyan@yahoo.com>
2020-11-23 23:14:40 -0800

    fixed projects

--- client/src/pages/Projects/index.jsx
+++ client/src/pages/Projects/index.jsx
@@ -17,10 +17,30 @@ function Projects(props) {
           <h1>PROJECTS</h1>
         </div>
         <div className="project-main">
-          <div className="planthood" onClick={()=> window.open("http://planthood.herokuapp.com/", "_blank")}><h3>PLANTHOOD</h3> <br/> </div>
+          <div
+            className="planthood"
+            onClick={() =>
+              window.open("http://planthood.herokuapp.com/", "_blank")
+            }
+          >
+            <h3>PLANTHOOD</h3> <br />{" "}
+          </div>
         </div>
-        <div className="project-main">  <div className="palmbuddy" onClick={()=> window.open("http://palmbuddy.herokuapp.com/", "_blank")}><h3 style={{opacity:0}}>--------------------</h3> <br/> </div></div>
-        <div className="github" onClick={ () => window.open("https://github.com/VartanyanE", "_blank")}></div>
+        <div className="project-main">
+          {" "}
+          <div
+            className="palmbuddy"
+            onClick={() =>
+              window.open("http://palmbuddy.herokuapp.com/", "_blank")
+            }
+          >
+            <h3 style={{ opacity: 0 }}>-------------</h3> <br />{" "}
+          </div>
+        </div>
+        <div
+          className="github"
+          onClick={() => window.open("https://github.com/VartanyanE", "_blank")}
+        ></div>
       </div>
     </>
   );
